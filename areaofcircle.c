86f5d1e7837033d3d04b40f818a44581e0cbd194
Gunjan564 <gunjan231103@gmail.com>
2023-11-10 21:20:59 +0530

    code

--- areaofcircle.c
+++ areaofcircle.c
@@ -1,12 +1,12 @@
 //Area of circle
-
 #include<stdio.h>
+#define PI 3.14
 int main ()
 {
     float a,r;
     printf("the value of r: ");
     scanf("%f",&r);
-    a=3.14*r*r;
+    a=PI*r*r;
     printf("the area of circle is %f",a);
     return 0;
 
