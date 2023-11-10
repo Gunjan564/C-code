54793492b8463ac130c1c2e6a86d44c4eb2ee97e
Gunjan564 <145559194+Gunjan564@users.noreply.github.com>
2023-11-10 11:34:55 +0000

    first commit

---
+++ escapesequencs.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("HELLO WORLD\n");//To skip a lie we use \n
+    printf("\tHELLO WORLD");//To give indentation we use \t
+    printf("\n\\\n");//To print a single back slash we use two backslash
+    printf("\\n");
+    return 0;
+}
