c77a4075cbb68b0f98f0323e2b11b4fe21bee9c2
Gunjan564 <145559194+Gunjan564@users.noreply.github.com>
2023-11-17 18:13:36 +0000

    first commit

--- escapesequencs.c
+++ escapesequencs.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    printf("HELLO WORLD\n");//To skip a lie we use \n
+    printf("HELLO WORLD\n");//To skip a line we use \n
     printf("\tHELLO WORLD");//To give indentation we use \t
     printf("\n\\\n");//To print a single back slash we use two backslash
     printf("\\n");
---
+++ intfloatoperation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    /*
+    == CASE 1(if both operrands are int or float then
+    the output will be stored only in int or float 
+    respectively.Even if the assigned values are in 
+    decimal the output will be in the input format 
+    specifier format. If the operation include another
+    term(decimal or real number)then the output will
+    depend on output format specifier).
+    NOTE:- 5/2 is accepteble in integer but 5.2 is not
+    ==CASE 2(if the input values are mixed between int and float then
+    the output will depend on output format specifier)*/
+    
+    
+}
