b70ce4431af2b1bd6562b5f5aa8b5db5577151fa
Gunjan564 <gunjan231103@gmail.com>
2023-11-18 00:56:46 +0530

    first commit

---
+++ assignmentoperator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=4;
+    //printf("one%d two%d",a,a++);
+    //printf("\none%d two%d",a++,++a);
+    //printf("\none%d two%d ",a,++a);
+    //printf("%d",++a);
+    printf("\none%d two%d",++a,++a);
+    return 0;
+}
