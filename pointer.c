f11da0a00c6ada2cd73938d53a69469200905069
Gunjan564 <145559194+Gunjan564@users.noreply.github.com>
2023-11-18 12:01:02 +0000

    first commit

--- pointer.c
+++ pointer.c
@@ -4,11 +4,11 @@ int main()
 {
     int a=7;
     int* pt=&a;
-    printf("hi%d",*pt);//value of the variable the adress of which 
-    printf("\nhi%p",&a);
-    printf("\nhi%p",&pt);
-    printf("\nhi%p",pt);
-    printf("\nhi%x\n",a);
+    printf("hi%d",*pt);//value of the variable the address of which stored in pointer
+    printf("\nhi%p",&a);//address of variable a
+    printf("\nhi%p",&pt);//address of pointer pt
+    printf("\nhi%p",pt);//address of variable a 
+    printf("\nhi%x\n",a);//value stored in a in hexadecimal form
     //%x is used to print hexadecimal value
     //%p is used to print address 
     return 0;
