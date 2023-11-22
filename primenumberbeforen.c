b17e90958e5f8144b8b7df9d041cbf13513e6db7
Gunjan564 <145559194+Gunjan564@users.noreply.github.com>
2023-11-22 06:15:36 +0000

    first commit

---
+++ primenumberbeforen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n, r, a;
+    printf("Enter a positive number: ");
+    scanf("%d", &n);
+    r = 0;
+    while (n != 1)
+    {
+        a = n;
+        for (i = 2; i < a; a / i, i++)
+        {
+
+            if (a % i == 0)
+            {
+                break;
+            }
+            else
+            {
+
+                if (i == n - 1)
+                {
+                    printf("%d ", n);
+                }
+                continue;
+            }
+        }
+        n = n - 1;
+        r++;
+    }
+    return 0;
+}
