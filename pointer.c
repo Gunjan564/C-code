d5813f8609f03f12b5c2606214bf3fc3fcc0550d
Gunjan564 <145559194+Gunjan564@users.noreply.github.com>
2023-11-24 03:29:34 +0530

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -25,6 +25,7 @@ int main()
     printf("\n%p",&arr[2]);//address of element present at arr[2];
     printf("\n%p",&arr);//address of first element of array
     printf("\n%d",*poi);//value of first element of array
+    printf("\n%d",poi[3]);//value of the element in array at arr[3]
     printf("\n%p",&x);//address of x
     printf("\n%d",x+y);
     //%x is used to print hexadecimal value
