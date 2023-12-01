4fb0cad5201262d8574316e3053c00801260cefc
Gunjan564 <145559194+Gunjan564@users.noreply.github.com>
2023-12-01 12:32:00 +0000

    first add

---
+++ multiplicationof2darray.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int row1,col1,row2,col2,arr[10][10];
+    for(row1=1;row1<=3;row1++)
+    {
+        for(col1=1;col1<=3;col1++)
+        {
+            printf("Enter the (%d,%d) element of array: ",row1,col1);
+            scanf("%d",&arr[row1][col1]);
+        }
+    }
+    printf("Following matrix is:\n");
+    for(row2=1;row2<=3;row2++)
+    {
+        for(col2=1;col2<=3;col2++)
+        {
+            printf("Enter the (%d,%d) element of array: ",row2,col2);
+            scanf("%d",&arr[row2][col2]);
+        }
+    }
+    printf("Following matrix is:\n");
+
+    return 0;
+}
