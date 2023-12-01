f8cf9b7ed89f197e1849c70bab23fb0f2201571f
Gunjan564 <145559194+Gunjan564@users.noreply.github.com>
2023-12-01 22:30:27 +0530

    add

--- multiplicationof2darray.c
+++ multiplicationof2darray.c
@@ -12,6 +12,14 @@ int main()
         }
     }
     printf("Following matrix is:\n");
+    for(row1=1;row1<=3;row1++)
+    {
+        for(col1=1;col1<=3;col1++)
+        {
+            printf("%d ",arr[row1][col1]);
+        }
+        printf("\n")
+    }
     for(row2=1;row2<=3;row2++)
     {
         for(col2=1;col2<=3;col2++)
@@ -21,6 +29,13 @@ int main()
         }
     }
     printf("Following matrix is:\n");
-
+    for(row2=1;row2<=3;row2++)
+    {
+        for(col2=1;col2<=3;col2++)
+        {
+            printf("%d ",arr[row2][col2]);
+        }
+        printf("\n")
+    }
     return 0;
 }
