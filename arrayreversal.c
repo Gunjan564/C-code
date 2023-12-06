d14e5e9e6b8e9cc5af809f34e3098f19e676805f
Gunjan <gunjan231103@gmail.comm>
2023-12-06 23:41:56 +0530

    cupid

--- arrayreversal.c
+++ arrayreversal.c
@@ -1,8 +1,22 @@
 #include<stdio.h>
+int rev(int arr[],int srr[],int i)
+{
+    int j;
+     for(i=i-1,j=0;i>=0;i--,j++)
+    {
+        arr[j]=srr[i];
+        printf("%d ",arr[j]);
+    }
+    return 0;
+}
 int main()
 {
-    int arr[9]="78968";
-    printf("Enter the values in array: %d",arr[]);
-    //scanf("%d",&arr[]); 
-          
+    int i,arr[9]={2,4,5,7,8,9},srr[9];
+    for(i=0;i<=5;i++)
+    {
+        printf("%d ",arr[i]);
+        srr[i]=arr[i];    
+    }
+    printf("\n");
+    rev(arr,srr,i);
 }
