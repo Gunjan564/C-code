23c4865e5f3b0da225eb320f0fcce7e1a92d0fb1
Gunjan <gunjan231103@gmail.comm>
2023-12-22 15:22:24 +0530

    message

--- reverseanumber.c
+++ reverseanumber.c
@@ -1,16 +1,40 @@
-//Reverse a number
-#include<stdio.h>
+// Reverse a number
+//  #include<stdio.h>
+//  int main()
+//  {
+//      int n,rev;
+//      printf("Enter a number: ");
+//      scanf("%d",&n);
+//      rev=0;
+//      while(n!=0)
+//      {
+//          rev=n%10+rev*10;
+//          n=n/10;
+//      }
+//      printf("%d",rev);
+//      return 0;
+//  }
+// Reverse a number using function
+#include <stdio.h>
+int exam;
+int rev(int *n)
+{
+    int r=0;
+    while(*n!=0)
+    {
+        
+        r = r * 10 + *n % 10;
+        *n = *n / 10;
+    }
+    // printf("%d",n);
+    return r;
+}
 int main()
 {
-    int n,rev;
-    printf("Enter a number: ");
-    scanf("%d",&n);
-    rev=0;
-    while(n!=0)
-    {
-        rev=n%10+rev*10;
-        n=n/10;
-    }
-    printf("%d",rev);
+    int a;
+    printf("Enter a no. to reverse it: ");
+    scanf("%d", &a);
+    printf("Reversed no. is: %d",rev(&a));
+    printf("\n%d",a);
     return 0;
 }
