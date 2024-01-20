846614f38fbadcbf4b58d7ab5105e2cc71c5e264
pulkitsujaan <pulkitsujaan2708@gmail.com>
2024-01-20 19:43:53 +0530

    skeptical

--- dynastruct.c
+++ dynastruct.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 int main()
 {
-    int i, n, e;
+    int i, n, e,ph,j;
     struct employ
     {
         char *id;
+        int phone;
     } ;
     printf("Enter the no. of employees:");
     scanf("%d", &e);
@@ -21,11 +22,19 @@ int main()
         getchar();
         printf("Enter the id: ");
         gets((ptr+i-1)->id);
+        printf("Enter the no. of digits in your phone numbers: ");
+        scanf("%d",&ph);
+        (ptr+i-1)->phone=(int*)malloc(ph*sizeof(int));
+        for(j=0;j<ph;j++)
+        {
+            scanf("%d",&(ptr+j-1)->phone);
+        }
     }
     for(i=1;i<=e;i++)
     {
         printf("Employ %d id: ",i);
         puts((ptr+i-1)->id);
     }
+
     return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,5 @@
+printf("Enter the no. of digits in your phone numbers: ");
+        // scanf("%d",&ph);
+        // {
+            
+        // }
