255ab028a7fd4e0cfc88ff78fb72ac328d11f1ec
pulkitsujaan <pulkitsujaan2708@gmail.com>
2024-01-22 10:36:36 +0530

    falling

--- pattern.c
+++ pattern.c
@@ -1,30 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int i,j,n;
+    int i, j, n;
     int ch;
     printf("Enter n: ");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
     {
-        if(i%2==0)
+        if (i % 2 == 0)
         {
-            for(j=1;j<=i;j++)
+            for (j = 1; j <= i; j++)
             {
-                ch=j+64;
-                printf("%c ",ch);
+                ch = j + 64;
+                printf("%c ", ch);
             }
             printf("\n");
         }
         else
         {
-            for(j=1;j<=i;j++)
+            for (j = 1; j <= i; j++)
             {
-                printf("%d ",j);
+                printf("%d ", j);
             }
             printf("\n");
         }
-        
     }
     return 0;
 }
