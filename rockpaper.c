ca5c3465d02ba2a6b28044fa1dee700f0222f09f
pulkitsujaan <pulkitsujaan2708@gmail.com>
2024-01-22 11:47:43 +0530

    paper

--- rockpaper.c
+++ rockpaper.c
@@ -1,22 +1,76 @@
-// Online C compiler to run C program online
 #include <stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include<string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 int main()
 {
-    // Write C code here
-    int n;
-    char name[30];
-    printf("Hello world\n");
-    srand(time(NULL));
-    printf("Your no. is: %d\n",rand()%4);
+    int player, comp, round, computer, you;
+    char name[30], move[9];
     printf("Enter your name: ");
     gets(name);
     printf("HELLO ");
     puts(name);
-    printf("Press 1 for ROCK, 2 for 2, 3 for SCISSORS");
-    scanf("%d",&n);
-    
+    for (round = 1, you = 0, computer = 0; round <= 3; round++)
+    {
+        printf("\nROUND %d\n", round);
+        printf("Press 1 for ROCK, 2 for PAPER, 3 for SCISSORS: ");
+        scanf("%d", &player);
+        if (player == 1)
+        {
+            strcpy(move, "ROCK");
+        }
+        if (player == 2)
+        {
+            strcpy(move, "PAPER");
+        }
+        if (player == 3)
+        {
+            strcpy(move, "SCISSORS");
+        }
+        printf("YOU: %s", move);
+        srand(time(NULL));
+        comp = rand() % 3;
+        if (comp == 0)
+        {
+            strcpy(move, "ROCK");
+        }
+        if (comp == 1)
+        {
+            strcpy(move, "PAPER");
+        }
+        if (comp == 2)
+        {
+            strcpy(move, "SCISSORS");
+        }
+        printf("\nComputer: %s\n", move);
+        // Computer wins
+        if ((comp == 0 && player == 3) || (comp == 1 && player == 1) || (comp == 2 && player == 2))
+        {
+            // printf("COMPUTER WON");
+            computer++;
+        }
+        if ((player == 1 && comp == 2) || (player == 2 && comp == 0) || (player == 3 && comp == 1))
+        {
+            // printf("YOU WON");
+            you++;
+        }
+        if ((player == 1 && comp == 0) || (player == 2 && comp == 1) || (player == 3 && comp == 2))
+        {
+            // printf("DRAW");
+            continue;
+        }
+    }
+    printf("\n");
+    if (computer > you)
+    {
+        printf("COMPUTER WON");
+    }
+    else if (you > computer)
+    {
+        printf("YOU WON");
+    }
+    else
+        printf("DRAW");
+
     return 0;
 }
